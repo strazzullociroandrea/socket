33ee941c08735cd9f3ae6a1c0e2b10e8f214094c
strazzullociroandrea <135723728+strazzullociroandrea@users.noreply.github.com>
2024-04-15 18:13:40 +0000

    Sistemazione con pagina di login

--- public/index.js
+++ public/index.js
@@ -1,54 +1,13 @@
-const input = document.getElementById("input");
-const button = document.getElementById("sendButton");
-const chat = document.getElementById("chat");
 const user = document.getElementById("user");
 const log = document.getElementById("log");
-const login = document.getElementById("login");
-const chatb = document.getElementById("chatb");
-
-const template = '<li class="list-group-item">%MESSAGE</li>';
-const messages = [];
-
-const socket = io();
-
-input.onkeydown = (event) => {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-    button.click();
-  }
-};
-
-button.onclick = () => {
-  socket.emit("message", input.value);
-  input.value = "";
-};
 
 log.onclick = () => {
-  if (user.value && user.value != "") {
+  const username = user.value;
+  user.value = "";
+  if (username && username != "") {
     user.classList.remove("border-danger");
-    socket.emit("login", user.value);
-    login.classList.add("d-none");
-    chatb.classList.remove("d-none");
+    window.location.href = "./socket/index.html?username="+username;
   } else {
     user.classList.add("border-danger");
   }
 };
-
-socket.on("chat", (message) => {
-  messages.push(message);
-  render();
-});
-
-const render = () => {
-  let html = "";
-  messages.forEach((message) => {
-    const row = template.replace("%MESSAGE", message);
-    html += row;
-  });
-  chat.innerHTML = html;
-  window.scrollTo(0, document.body.scrollHeight);
-};
-
-window.addEventListener("beforeunload", function (event) {
-  socket.emit("disconnecter");
-});
---
+++ public/socket/index.js
@@ -0,0 +1,53 @@
+//dom ed oggetti
+const input = document.getElementById("input");
+const button = document.getElementById("sendButton");
+const chat = document.getElementById("chat");
+const user = document.getElementById("user");
+const disconnetti = document.getElementById("disconnetti");
+
+const template = '<li class="list-group-item">%MESSAGE</li>';
+const messages = [];
+const socket = io();
+const url = new URL(window.location.href);
+const username = url.searchParams.get("username");
+
+//funzioni
+const render = () => {
+  let html = "";
+  messages.forEach((message) => {
+    const row = template.replace("%MESSAGE", message);
+    html += row;
+  });
+  chat.innerHTML = html;
+  window.scrollTo(0, document.body.scrollHeight);
+};
+
+//al caricamento della pagina gestisco il login
+window.onload = () => {
+  if (username && username != "") {
+    socket.emit("login", username);
+  } else {
+    window.location.href = "./../";
+  }
+};
+
+input.onkeydown = (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    button.click();
+  }
+};
+
+button.onclick = () => {
+  socket.emit("message", input.value);
+  input.value = "";
+};
+socket.on("chat", (message) => {
+  messages.push(message);
+  render();
+});
+
+disconnetti.onclick = () => {
+  socket.emit("disconnecter");
+  window.location.href = "./../";
+};
